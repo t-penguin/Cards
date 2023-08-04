fb0387ab9409b931d439f435bdc31074e912ba5b
Joseph Collado <sourpina@gmail.com>
2023-08-04 19:52:50 -0400

    Created Script
    Created a script to hold values for a card

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card
+{
+    [field: SerializeField] public Suit Suit { get; private set; }
+    [field: SerializeField] public FaceValue FaceValue { get; private set; }
+    [field: SerializeField] public int NumberValue { get; private set; }
+
+    public Card(Suit suit, FaceValue fValue)
+    {
+        Suit = suit;
+        FaceValue = fValue;
+        NumberValue = (int)FaceValue;
+    }
+}
+
+public enum Suit
+{
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs
+}
+
+public enum FaceValue
+{
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14
+}
