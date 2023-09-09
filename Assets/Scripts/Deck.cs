8cd3ae9ac89a78f4c59227ed73f02161c66dea11
Joseph Collado <sourpina@gmail.com>
2023-09-09 16:48:16 -0400

    Added condition
    Added a condition to the Deal to player validation if the number of cards to give is not a factor of the remaining cards for whatever reason.

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -6,10 +6,10 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
     public static event Action<DeckType> DeckCreated;
-    
 
     [field: SerializeField] public Card[] Cards { get; private set; }
 
+    // Creates an array of cards (the deck) using the suit and face value enums
     public void CreateDeck(DeckType type)
     {
         Cards = new Card[52];
@@ -46,6 +46,12 @@ public class Deck : MonoBehaviour
         Debug.Log("Deck shuffled!");
     }
 
+    /// <summary>
+    /// Deals cards to a set of players
+    /// </summary>
+    /// <param name="players">The list of players currently in the game.</param>
+    /// <param name="playerOffset">The offset used to determine who gets dealt to first.</param>
+    /// <param name="deckOffset">The offset used to determine where in the deck to deal from.</param>
     public void Deal(List<Player> players, int playerOffset, int deckOffset)
     {
         if(Cards == null)
@@ -63,7 +69,7 @@ public class Deck : MonoBehaviour
 
         int cardsToGive = numPlayers * 4;
         int remainingCards = 52 - deckOffset;
-        if(cardsToGive > remainingCards)
+        if(cardsToGive > remainingCards || remainingCards % cardsToGive != 0)
         {
             Debug.LogError($"Attempted to deal but the number of remaining cards ({remainingCards}) is invalid!");
             return;
