4e845d602a85ded282057263820589af655e0908
Joseph Collado <sourpina@gmail.com>
2024-02-11 17:25:16 -0500

    Update Card.cs
    - Added static dictionaries for quick lookup for Suit and FaceValue enums using strings
    - Added new constructor using string

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,14 +5,56 @@ using UnityEngine;
 [System.Serializable]
 public class Card
 {
+    #region Static Properties
+
+    public static Dictionary<string, Suit> SuitDictionary = 
+        new()
+        {
+            { "Hearts", Suit.Hearts },
+            { "Diamonds", Suit.Diamonds },
+            { "Spades", Suit.Spades },
+            { "Clubs", Suit.Clubs }
+        };
+
+    public static Dictionary<string, FaceValue> FaceValueDictionary =
+        new()
+        {
+            { "Two", FaceValue.Two },
+            { "Three", FaceValue.Three },
+            { "Four", FaceValue.Four },
+            { "Five", FaceValue.Five },
+            { "Six", FaceValue.Six },
+            { "Seven", FaceValue.Seven },
+            { "Eight", FaceValue.Eight },
+            { "Nine", FaceValue.Nine },
+            { "Ten", FaceValue.Ten },
+            { "Jack", FaceValue.Jack },
+            { "Queen", FaceValue.Queen },
+            { "King", FaceValue.King },
+            { "Ace", FaceValue.Ace}
+        };
+
+    #endregion
+
     [field: SerializeField] public Suit Suit { get; private set; }
     [field: SerializeField] public FaceValue FaceValue { get; private set; }
     [field: SerializeField] public int NumberValue { get; private set; }
+    public string Name { get { return $"{Suit}_{FaceValue}"; } }
 
     public Card(Suit suit, FaceValue fValue)
     {
         Suit = suit;
         FaceValue = fValue;
+        NumberValue = (int)FaceValue;
+    }
+
+    public Card(string name)
+    {
+        string suitName = name.Split('_')[0];
+        string faceValue = name.Split("_")[1];
+
+        Suit = SuitDictionary[suitName];
+        FaceValue = FaceValueDictionary[faceValue];
         NumberValue = (int)FaceValue;
     }
 
@@ -22,6 +64,7 @@ public class Card
     }
 }
 
+[System.Serializable]
 public enum Suit
 {
     Hearts,
@@ -30,6 +73,7 @@ public enum Suit
     Clubs
 }
 
+[System.Serializable]
 public enum FaceValue
 {
     Two = 2,
