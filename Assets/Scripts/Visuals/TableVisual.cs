28a31b0f8ad63208560cc1a7b2e62ff62db0750b
Joseph Collado <sourpina@gmail.com>
2024-02-19 03:34:16 -0500

    Update TableVisual.cs
    Added event handlers for:
    - Dropped Cards
    - Stacked Cards (placeholder)
    - Took Cards (placeholder)
    Made player count a global private variable to be used elsewhere.
    Added function for dropping cards onto the table.
    Added functions for checking and resizing the table cards when certain card amount thresholds are crossed.
    Changed Deal To Table to only occur on the first round.
    Changed the Turn Start event to only be raised by the master client instead of everyone.

--- Assets/Scripts/Visuals/TableVisual.cs
+++ Assets/Scripts/Visuals/TableVisual.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Photon;
-using Unity.VisualScripting.Antlr3.Runtime;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
-using ExitGames.UtilityScripts;
 
 public class TableVisual : PunBehaviour
 {
@@ -47,8 +45,10 @@ public class TableVisual : PunBehaviour
         }
     }
 
-    public static void LogPlayerHands(PhotonPlayer[] players)
+    public static void LogPlayerHands()
     {
+        PhotonPlayer[] players = (PhotonPlayer[])PhotonNetwork.room.CustomProperties[GameManager.TURN_ORDER_KEY];
+
         foreach (PhotonPlayer player in players)
         {
             string log = $"{player.NickName}:\n";
@@ -76,6 +76,16 @@ public class TableVisual : PunBehaviour
         }
     }
 
+    public static void LogTurnOrder()
+    {
+        PhotonPlayer[] TurnOrder = (PhotonPlayer[])PhotonNetwork.room.CustomProperties[GameManager.TURN_ORDER_KEY];
+        string order = "Turn Order:\n";
+        foreach (PhotonPlayer player in TurnOrder)
+            order += $"{player.NickName}\n";
+
+        Debug.Log(order);
+    }
+
     #endregion
 
     public const byte TABLE_SET_EVENT_CODE = 2;
@@ -96,7 +106,24 @@ public class TableVisual : PunBehaviour
     List<GameObject> _activePlayerPanels;
     List<RectTransform> _activePlayerHands;
 
+    int _playerCount;
     int _tablePlayerOffset;
+
+    TableSize _tableSize;
+    Vector2 _defaultTableSize = new Vector2(550, 575);
+    Vector2 _mediumTableSize = new Vector2(820, 575);
+    Vector2 _largeTableSize = new Vector2(890, 575);
+
+    Vector2 _defaultCellSize = new Vector2(125, 175);
+    Vector2 _smallCellSize = new Vector2(100, 140);
+    Vector2 _teenyCellSize = new Vector2(75, 105);
+
+    Vector2 _defaultSelectorSize = new Vector2(140, 190);
+    Vector2 _smallSelectorSize = new Vector2(115, 155);
+    Vector2 _teenySelectorSize = new Vector2(82, 112);
+
+    Vector2 _defaultSpacing = new Vector2(10, 10);
+    Vector2 _smallSpacing = new Vector2(5, 10);
 
     #region Monobehaviour Callbacks
 
@@ -104,12 +131,14 @@ public class TableVisual : PunBehaviour
     {
         PhotonNetwork.OnEventCall += OnTurnOrderSet;
         PhotonNetwork.OnEventCall += OnGameSetStarted;
+        PhotonNetwork.OnEventCall += OnDroppedCard;
     }
 
     private void OnDisable()
     {
         PhotonNetwork.OnEventCall -= OnTurnOrderSet;
         PhotonNetwork.OnEventCall -= OnGameSetStarted;
+        PhotonNetwork.OnEventCall -= OnDroppedCard;
     }
 
     #endregion
@@ -136,6 +165,25 @@ public class TableVisual : PunBehaviour
         StartCoroutine(StartDealing());
     }
 
+    private void OnDroppedCard(byte eventCode, object content, int senderId)
+    {
+        if (eventCode != TurnManager.TURN_DROP_CARD_EVENT_CODE)
+            return;
+
+        int[] dropInfo = (int[])content;
+        StartCoroutine(DropCard(dropInfo));
+    }
+
+    private void OnStackedCards(byte eventCode, object content, int senderId)
+    {
+
+    }
+
+    private void OnTookCards(byte eventCode, object content, int senderId)
+    {
+
+    }
+
     #endregion
 
     #region Table Set Up
@@ -147,7 +195,7 @@ public class TableVisual : PunBehaviour
         PhotonPlayer[] TurnOrder = 
             (PhotonPlayer[])PhotonNetwork.room.CustomProperties[GameManager.TURN_ORDER_KEY];
 
-        int playerCount = TurnOrder.Length;
+        _playerCount = TurnOrder.Length;
         _tablePlayerOffset = Array.IndexOf(TurnOrder, PhotonNetwork.player);
 
         if (_tablePlayerOffset == -1)
@@ -161,10 +209,10 @@ public class TableVisual : PunBehaviour
         foreach (GameObject panel in _playerPanels)
             panel.SetActive(false);
 
-        switch (playerCount)
+        switch (_playerCount)
         {
             default:
-                Debug.LogError($"Invalid number of players... Got {playerCount} but expected 2, 3, or 4.");
+                Debug.LogError($"Invalid number of players... Got {_playerCount} but expected 2, 3, or 4.");
                 return;
             case 2: // Active Panels: Bottom, Top
                 AddActive(BOTTOM);
@@ -183,9 +231,9 @@ public class TableVisual : PunBehaviour
                 break;
         }
 
-        for (int i = 0; i < playerCount; i++)
+        for (int i = 0; i < _playerCount; i++)
         {
-            PhotonPlayer player = TurnOrder[(i + _tablePlayerOffset) % playerCount];
+            PhotonPlayer player = TurnOrder[(i + _tablePlayerOffset) % _playerCount];
             // TODO: [CARDS] Correctly set the player ID image here
             _activePlayerPanels[i].GetComponentInChildren<IDCardVisual>()
                 .SetIDCard(player.NickName, null);
@@ -204,11 +252,22 @@ public class TableVisual : PunBehaviour
                 sendReliable: true, GameManager.EventOptions);
     }
 
+    #endregion
+
+    #region Table Animations
+
     // Waits until the players have been dealt their cards, then deals to the table
     private IEnumerator StartDealing()
     {
         yield return StartCoroutine(DealToPlayers());
-        yield return StartCoroutine(DealToTable());
+
+        int round = (int)PhotonNetwork.room.CustomProperties[GameManager.CURRENT_ROUND_KEY];
+        if (round == 1)
+            yield return StartCoroutine(DealToTable());
+
+        if (PhotonNetwork.isMasterClient)
+            PhotonNetwork.RaiseEvent(TurnManager.TURN_STARTED_EVENT_CODE, eventContent: null,
+                sendReliable: true, GameManager.EventOptions);
     }
 
     // Visually deals cards from the deck to the players
@@ -250,6 +309,7 @@ public class TableVisual : PunBehaviour
     private IEnumerator DealToTable()
     {
         string[] cardNames = (string[])PhotonNetwork.room.CustomProperties[GameManager.TABLE_HAND_KEY];
+        SetTableSize(TableSize.Default);
 
         for (int i = 0; i < 4; i++)
         {
@@ -263,7 +323,7 @@ public class TableVisual : PunBehaviour
 
         // Create a card at the deck location face down
         GameObject cardVisual = Instantiate(_cardVisualPrefab, _deck);
-        cardVisual.transform.SetParent(_deck.parent.transform);
+        cardVisual.transform.SetParent(_deck.parent);
         cardVisual.SetActive(false);
 
         RectTransform cardVisualTransform = cardVisual.GetComponent<RectTransform>();
@@ -281,11 +341,14 @@ public class TableVisual : PunBehaviour
 
         yield return MoveAndRotateCard(cardVisualTransform, targetPosition, targetRotation);
 
-        cardVisualTransform.anchoredPosition = targetPosition;
-        cardVisualTransform.eulerAngles = targetRotation;
+        //cardVisualTransform.anchoredPosition = targetPosition;
+        //cardVisualTransform.eulerAngles = targetRotation;
 
         // Parent the card to the target hand
         cardVisualTransform.SetParent(targetHand);
+
+        CardInteraction cardInteraction = cardVisual.GetComponent<CardInteraction>();
+        cardInteraction.Card = new Card(cardName);
 
         if (!flip)
             yield break;
@@ -295,9 +358,48 @@ public class TableVisual : PunBehaviour
         cardVisualImage.sprite = cardSprite;
 
         // Cards that are flipped are also interactable
-        CardInteraction cardInteraction = cardVisual.GetComponent<CardInteraction>();
         cardInteraction.CardType = targetHand == _tableHand ? CardType.OnTable : CardType.InHand;
-        cardInteraction.Card = new Card(cardName);
+
+        yield break;
+    }
+
+    private IEnumerator DropCard(int[] dropInfo)
+    {
+        int senderIndex = dropInfo[0];
+        Debug.Log($"Sender index in the turn order is: {senderIndex}");
+        int senderTableIndex = (senderIndex + _tablePlayerOffset) % _playerCount;
+        RectTransform senderHand = _activePlayerHands[senderTableIndex];
+        CardInteraction[] senderCards = senderHand.GetComponentsInChildren<CardInteraction>();
+
+        int cardIndex = dropInfo[1];
+        Debug.Log($"Card index in sender's hand is: {cardIndex}");
+        CardInteraction card = senderCards[cardIndex];
+        RectTransform cardTransform = card.GetComponent<RectTransform>();
+
+        Vector3 targetPosition = _tableHand.anchoredPosition;
+        Vector3 targetRotation = _tableHand.eulerAngles;
+
+        cardTransform.SetParent(_deck.parent);
+        yield return StartCoroutine(MoveAndRotateCard(cardTransform, targetPosition, targetRotation));
+        cardTransform.SetParent(_tableHand);
+        CheckTableSize();
+
+        if (_tableSize == TableSize.ExtraLarge)
+            card.ResizeSelector(_teenySelectorSize);
+        else if (_tableSize == TableSize.Large)
+            card.ResizeSelector(_smallSelectorSize);
+        else
+            card.ResizeSelector(_defaultSelectorSize);
+        
+        Image cardImage = card.GetComponent<Image>();
+        string cardName = card.Card.Name;
+        Sprite cardSprite = GameManager.GetCardSpriteByName(cardName);
+        cardImage.sprite = cardSprite;
+        card.CardType = CardType.OnTable;
+
+        if(PhotonNetwork.isMasterClient)
+            PhotonNetwork.RaiseEvent(TurnManager.TURN_ENDED_EVENT_CODE, eventContent: null,
+                sendReliable: true, GameManager.EventOptions);
 
         yield break;
     }
@@ -305,6 +407,9 @@ public class TableVisual : PunBehaviour
     private IEnumerator MoveAndRotateCard(RectTransform cardTransform, Vector3 targetPosition, Vector3 targetRotation)
     {
         Debug.Log("Move and Rotate NYI...");
+        cardTransform.anchoredPosition = targetPosition;
+        cardTransform.eulerAngles = targetRotation;
+
         yield break;
     }
 
@@ -315,4 +420,77 @@ public class TableVisual : PunBehaviour
         _activePlayerHands.Add(_playerHands[index]);
         _activePlayerPanels.Add(_playerPanels[index]);
     }
+
+    private void CheckTableSize()
+    {
+        CardInteraction[] tableCards = _tableHand.GetComponentsInChildren<CardInteraction>();
+        int numCards = tableCards.Length;
+
+        Debug.Log($"There are {numCards} cards on the table");
+
+        TableSize targetSize;
+        if (numCards > 24)
+            targetSize = TableSize.ExtraLarge;
+        else if (numCards > 18)
+            targetSize = TableSize.Large;
+        else if (numCards > 12)
+            targetSize = TableSize.Medium;
+        else
+            targetSize = TableSize.Default;
+
+        Debug.Log($"Table size: {_tableSize} / Target size: {targetSize}");
+
+        if (_tableSize != targetSize)
+            SetTableSize(targetSize, tableCards);
+    }
+
+    private void SetTableSize(TableSize size, CardInteraction[] cards = null)
+    {
+        GridLayoutGroup layout = _tableHand.GetComponent<GridLayoutGroup>();
+        _tableSize = size;
+
+        Vector2 selectorSize = _defaultSelectorSize;
+
+        switch (size)
+        {
+            case TableSize.Default:
+                _tableHand.sizeDelta = _defaultTableSize;
+                layout.cellSize = _defaultCellSize;
+                layout.spacing = _defaultSpacing;
+                break;
+            case TableSize.Medium:
+                _tableHand.sizeDelta = _mediumTableSize;
+                layout.cellSize = _defaultCellSize;
+                layout.spacing = _defaultSpacing;
+                break;
+            case TableSize.Large:
+                _tableHand.sizeDelta = _largeTableSize;
+                selectorSize = _smallSelectorSize;
+                layout.cellSize = _smallCellSize;
+                layout.spacing = _defaultSpacing;
+                break;
+            case TableSize.ExtraLarge:
+                _tableHand.sizeDelta = _largeTableSize;
+                selectorSize = _teenySelectorSize;
+                layout.cellSize = _teenyCellSize;
+                layout.spacing = _smallSpacing;
+                break;
+        }
+
+        if (cards == null)
+            return;
+
+        foreach (CardInteraction card in cards)
+            card.ResizeSelector(selectorSize);
+
+        Debug.Log($"Selector size should be {selectorSize.x}x{selectorSize.y}");
+    }
+}
+
+enum TableSize
+{
+    Default,
+    Medium,
+    Large,
+    ExtraLarge
 }
