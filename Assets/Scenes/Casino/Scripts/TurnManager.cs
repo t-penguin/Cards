16fbca1ae01aace9458eadd60aa48264720d9532
Joseph Collado <sourpina@gmail.com>
2024-02-19 03:15:11 -0500

    Implemented some turn events
    Added events for:
    - Turn Start
    - Turn End
    - Drop Card
    
    Added event handlers for:
    - Set started
    - Turn started
    - Turn ended
    
    Refined the ability to select cards.
    Added the ability to group together cards that add up to the target value.
    Added the ability to drop a card to the table and play that as a turn.

--- Assets/Scenes/Casino/Scripts/TurnManager.cs
+++ Assets/Scenes/Casino/Scripts/TurnManager.cs
@@ -1,56 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using TMPro;
+using System;
+using System.Linq;
 
 public class TurnManager : MonoBehaviour
 {
     // TODO: [CARDS] Figure out turn actions and events
+    // TODO: [CARDS] Add clear stacks button
 
     private const string TARGET_VALUE_TEXT = "Target Value:";
 
+    public const byte TURN_STARTED_EVENT_CODE = 6;
+    public const byte TURN_ENDED_EVENT_CODE = 7;
+    public const byte TURN_DROP_CARD_EVENT_CODE = 8;
+
+    [SerializeField] GameObject _playerHand;
     [SerializeField] GameObject _buttonsPanel;
     [SerializeField] TextMeshProUGUI _targetValueText;
 
+    List<CardInteraction> _playerCards;
     List<CardInteraction> _selectedTableCards;
+    List<CardInteraction> _stackedCards;
     CardInteraction _selectedHandCard;
     CardInteraction _targetValueCard;
     int _targetValue;
+    bool _handCardInStack;
+
+    PhotonPlayer[] TurnOrder;
+    int _playerTurnIndex;
+    int _currentTurnIndex;
+
+    Color _redOutline = new Color(0.5f, 0f, 0f);
+    Color _blueOutline = new Color(0f, 0f, 0.5f);
+    Color _blackOutline = new Color(0f, 0f, 0f);
 
     #region Monobehaviour Callbacks
 
     private void Awake()
     {
         _selectedTableCards = new List<CardInteraction>();
-
-        // TODO: [CARDS] Remove this once the events are set up
-        OnTurnStarted();
+        _stackedCards = new List<CardInteraction>();
     }
 
     private void OnEnable()
     {
-        CardInteraction.ClickedCard += OnClickedCard; 
+        CardInteraction.ClickedCard += OnClickedCard;
+        PhotonNetwork.OnEventCall += OnSetStarted;
+        PhotonNetwork.OnEventCall += OnTurnStarted;
+        PhotonNetwork.OnEventCall += OnTurnEnded;
     }
 
     private void OnDisable()
     {
         CardInteraction.ClickedCard -= OnClickedCard;
+        PhotonNetwork.OnEventCall -= OnSetStarted;
+        PhotonNetwork.OnEventCall -= OnTurnStarted;
+        PhotonNetwork.OnEventCall -= OnTurnEnded;
     }
 
     #endregion
 
     #region PUN Event Callbacks
 
-    private void OnTurnStarted()
+    private void OnSetStarted(byte eventCode, object content, int senderID)
     {
+        if (eventCode != GameManager.GAME_SET_STARTED)
+            return;
+
+        TurnOrder = (PhotonPlayer[])PhotonNetwork.room.CustomProperties[GameManager.TURN_ORDER_KEY];
+        _playerTurnIndex = Array.IndexOf(TurnOrder, PhotonNetwork.player);
+    }
+
+    private void OnTurnStarted(byte eventCode, object content, int senderId)
+    {
+        if (eventCode != TURN_STARTED_EVENT_CODE)
+            return;
+
+        if (_playerCards == null || _playerCards.Count == 0)
+            _playerCards = _playerHand.GetComponentsInChildren<CardInteraction>().ToList();
+
+        int currentTurn = PhotonNetwork.room.GetTurn();
+        _currentTurnIndex = currentTurn % TurnOrder.Length;
+
+        if (_currentTurnIndex != _playerTurnIndex)
+            return;
+
         _targetValueText.text = TARGET_VALUE_TEXT;
         _targetValueText.gameObject.SetActive(true);
     }
 
-    private void OnTurnEnded()
+    private void OnTurnEnded(byte eventCode, object content, int senderId)
     {
-        _targetValueText.gameObject.SetActive(false );
+        if(eventCode != TURN_ENDED_EVENT_CODE)
+            return;
+
+        if (_currentTurnIndex != _playerTurnIndex)
+            return;
+
+        _targetValueText.gameObject.SetActive(false);
     }
 
     #endregion
@@ -59,6 +110,9 @@ public class TurnManager : MonoBehaviour
 
     private void OnClickedCard(CardInteraction cardInteraction, PointerEventData eventData)
     {
+        if(_currentTurnIndex != _playerTurnIndex)
+            return;
+
         CardType cardType = cardInteraction.CardType;
 
         if (cardType == CardType.InHand)
@@ -73,9 +127,13 @@ public class TurnManager : MonoBehaviour
 
     private void HandleHandClick(CardInteraction cardInteraction, PointerEventData eventData)
     {
-        bool alreadySelected = cardInteraction.IsSelected;
+        if (_handCardInStack)
+            return;
 
-        if (alreadySelected)
+        bool alreadySelected = cardInteraction.IsSelected;
+        bool handCardIsTarget = _selectedHandCard == _targetValueCard;
+
+        if (alreadySelected && !handCardIsTarget)
         {
             // Deselect this card
             _selectedHandCard = null;
@@ -85,8 +143,8 @@ public class TurnManager : MonoBehaviour
             return;
         }
 
-        // Deselect the previous card if one has been selected
-        if (_selectedHandCard != null)
+        // Deselect the previous card if one has been selected unless it's the target value
+        if (_selectedHandCard != null && !handCardIsTarget)
         {
             _selectedHandCard.IsSelected = false;
             _selectedHandCard.HideSelector();
@@ -105,18 +163,23 @@ public class TurnManager : MonoBehaviour
 
     private void HandleTableClick(CardInteraction cardInteraction, PointerEventData eventData)
     {
+        bool cardInStack = false;
+        if(_stackedCards != null)
+            cardInStack = _stackedCards.Contains(cardInteraction);
+
+        if (cardInStack)
+            return;
+
         bool alreadySelected = cardInteraction.IsSelected;
 
         if (alreadySelected)
         {
             // Deselect this card
-            _selectedTableCards.Remove(cardInteraction);
-            cardInteraction.IsSelected = false;
+            DeselectTableCard(cardInteraction);
             return;
         }
 
-        _selectedTableCards.Add(cardInteraction);
-        cardInteraction.IsSelected = true;
+        SelectTableCard(cardInteraction);
     }
 
     #endregion
@@ -125,19 +188,112 @@ public class TurnManager : MonoBehaviour
 
     public void DropCard()
     {
-        // TODO: [CARDS] Send Drop Card event to the game manager that sends event over PUN
         string cardName = _selectedHandCard.Card.Name;
         Debug.Log($"Selected to drop the {_selectedHandCard.Card}");
+
+        _selectedHandCard.IsSelected = false;
+        _selectedHandCard.HideSelector();
+        SetSelectorColor(_selectedHandCard, _blackOutline);
+
+        if(_targetValueCard != null)
+        {
+            _targetValueCard.IsSelected = false;
+            _targetValueCard.HideSelector();
+            SetSelectorColor(_targetValueCard, _blackOutline);
+            _targetValueCard = null;
+        }
+
+        foreach(CardInteraction card in _selectedTableCards)
+        {
+            card.IsSelected = false;
+            card.HideSelector();
+        }
+        _selectedTableCards.Clear();
+
+        int cardIndex = _playerCards.IndexOf(_selectedHandCard);
+        int[] dropInfo = { _playerTurnIndex, cardIndex };
+        _playerCards.Remove(_selectedHandCard);
+
+        _buttonsPanel.SetActive(false);
+
+        PhotonNetwork.RaiseEvent(TURN_DROP_CARD_EVENT_CODE, eventContent: dropInfo,
+            sendReliable: true, GameManager.EventOptions);
     }
 
     // Sets the selected card as the target value and updates the text
     public void SetCardAsTargetValue()
     {
-        // TODO: [CARDS] Set the color of the selector to red if it's the target value
+        if(_targetValueCard != null)
+        {
+            _targetValueCard.IsSelected = false;
+            _targetValueCard.HideSelector();
+            SetSelectorColor(_targetValueCard, _blackOutline);
+        }
+
         _targetValueCard = _selectedHandCard;
         _targetValue = _targetValueCard.Card.NumberValue;
         _targetValueText.text = $"{TARGET_VALUE_TEXT} {_targetValue}";
+
+        SetSelectorColor(_targetValueCard, _redOutline);
+    }
+
+    public void StackCards()
+    {
+        // Total up the value of all the selected cards
+        int stackValue = 0;
+
+        foreach (CardInteraction cardInteraction in _selectedTableCards)
+        {
+            stackValue += cardInteraction.Card.NumberValue;
+        }
+
+        // Do NOT include the target value card,
+        // but do include the selected card in hand if it's not the target value
+        bool selectedCardIsTarget = _selectedHandCard == _targetValueCard;
+        if (!selectedCardIsTarget)
+        {
+            stackValue += _selectedHandCard.Card.NumberValue;
+        }
+         
+        if (stackValue != _targetValue)
+        {
+            // Invalid selection
+            Debug.Log($"Stack value of {stackValue} does not match the target value of {_targetValue}");
+            return;
+        }
+
+        foreach (CardInteraction cardInteraction in _selectedTableCards)
+        {
+            SetSelectorColor(cardInteraction, _blueOutline);
+            _stackedCards.Add(cardInteraction);
+        }
+
+        if(!selectedCardIsTarget)
+        {
+            SetSelectorColor(_selectedHandCard, _blueOutline);
+            _stackedCards.Add(_selectedHandCard);
+        }
+
+         _selectedTableCards.Clear();
     }
 
     #endregion
+
+    private void SelectTableCard(CardInteraction card)
+    {
+        _selectedTableCards.Add(card);
+        card.IsSelected = true;
+    }
+
+    private void DeselectTableCard(CardInteraction card)
+    {
+        _selectedTableCards.Remove(card);
+        card.IsSelected = false;
+    }
+
+    private void SetSelectorColor(CardInteraction card, Color color)
+    {
+        Image selectorImage = card.GetSelectorImage();
+        selectorImage.color = color;
+    }
 }
